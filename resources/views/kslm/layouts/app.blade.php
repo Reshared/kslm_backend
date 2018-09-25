f7091f66386b8e75608e69827d94368102183380
reshared <houreshared@gmail.com>
2018-09-25 10:57:59 +0800

    移动端

--- resources/views/kslm/layouts/app.blade.php
+++ resources/views/kslm/layouts/app.blade.php
@@ -6,8 +6,8 @@
 @section('description')Koch Membrane Systems Inc系一家注册于美国堪萨斯州的美国公司。专注于注册商标为“KOCH”和“科氏”的RO反渗透膜，NF纳滤膜，UF中空超滤膜，卷式超滤膜，微滤膜等膜制品的研发。己拥有数十年的滤膜制造和应用经验，已经真正成为膜分离领域具有最完善产品链的知名品牌。系全球最大的滤膜供应商之一。能够提供从微滤、超滤、纳滤和反渗透等各种过滤精度的膜产品及系统，膜件结构形式包括中空纤维式、卷式、管式三种。Koch Membrane Systems Inc极为重视亚太水处理市场，尤其是水处理业务高速增长的中国市场，2014年在华成立科氏（北京）滤膜科技有限公司，负责在华的所有业务。科氏（KOCH)公司拥有世界上最完善的研发设备和先进的膜生产检测系统：①100%的完整性检测；②USP四级标准毒性检测；③符合FDA标准。科氏（KOCH）公司提供的不同结构（中空、卷式、管式、板式）、不同材质（PS，PVDF，PAN，PES等）、不同过滤精度（从微滤、超滤、纳滤到反渗透全面的分离级 别）和不同应用标准的滤膜。产品己经广泛应用于废水、工业、民用、饮用水处理、医药、生化技术、食品、乳制品、饮料、电泳涂装等各个行业，KOCH滤膜产品己成为先进生产科技的代表。本网站建设由新墨提供技术支持，新墨提供APP开发、微信小程序开发、物联网开发、区块链开发。新墨电话：010-80444845，新墨官网：http://www.sinmore.com.cn
 @stop
 @section('css')
-    <script src="{{ 'js/lib/flexible_css.debug.js' }}"></script>
-    <script src="{{ 'js/lib/flexible.debug.js' }}"></script>
+    <script src="{{ asset('js/lib/flexible_css.debug.js') }}"></script>
+    <script src="{{ asset('js/lib/flexible.debug.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('css/lib/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/lib/animate.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/lib/swiper-3.4.2.min.css') }}">
