d7b2ac82d54b5f1d7589125eae5638fddca22847
reshared <reshared@home.com>
2018-10-13 18:43:19 +0800

    add migration

--- app/Backend/Controllers/ProductController.php
+++ app/Backend/Controllers/ProductController.php
@@ -75,7 +75,7 @@ class ProductController extends Controller
             $form->number('sort', '序号值');
             $form->listbox('posts', '关联文章')->options($postArr)->help('左侧为待选资讯列表；右侧为已关联资讯列表');
             $form->ckeditor('content', '产品详情')->rules('required');
-            $form->multipleImage('files','附件')->removable();
+            $form->multipleFile('files','附件')->removable();
 //            $form->multipleFile('files', '附件上传')->options([
 //                'uploadUrl' => Input::url() . '/upload_file',
 //                'deleteUrl' => Input::url() . '/un_upload_file',
